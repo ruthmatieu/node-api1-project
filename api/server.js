e6c06c00cb89f3bfce55beba597e52acacd6fd5d
matieuruth <matieu.ruth@gmail.com>
2021-02-11 22:42:06 -0500

    .

--- api/server.js
+++ api/server.js
@@ -2,15 +2,48 @@
 const express = require('express');
 const database = require('./users/model');
 const server = express();
+const users = database;
 server.use(express.json());
 
 server.get('/', (req, res) => {
     res.json({ message: `You've reached the right place`})
 });
 
-server.get('/users', (req, res) => {
-    const users = database.getUsers();
-    res.json(users)
+server.get('/api/users', (req, res) => {
+    
+    users.find()
+        .then((users) => {
+        res.status(200).json(users)
+
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({
+                message: 'Error'
+            })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    
+    users.findById(req.params.id)
+        .then((user) => {
+            if(user){
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({
+					message: "User not found",
+				})
+            }
+            
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({
+                message: 'That user does not exist.'
+            })
+        })
+    
 })
 
 module.exports = {server}; // EXPORT YOUR SERVER instead of {}
